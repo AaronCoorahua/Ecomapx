b35db2d0036cb517cd5640e205b73637a601451b
Aaron Coorahua <ruben.coorahua@utec.edu.pe>
2023-10-04 19:46:23 -0500

    fix: cambio de datos en views.py a key_config.py

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,12 @@
-from flask import Flask
-#from flask_session import Session
-from flask_dynamo import Dynamo
-from flask_cors import CORS
-import key_config as keys
-import boto3
-import os
-print("Hola Mundo")
+from flask import Flask, session, redirect, url_for, escape, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        return 'Logged in as %s' % escape(session['username'])
+    return 'You are not logged in'
+
+if __name__ == '__main__':
+    app.run(debug=True)
