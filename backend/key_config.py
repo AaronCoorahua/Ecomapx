004a4585103f3e264333c75ebfe138746c9b154b
Aaron Coorahua <ruben.coorahua@utec.edu.pe>
2023-10-05 13:02:49 -0500

    fix: cambios en las credenciales y en el views.py

--- backend/key_config.py
+++ backend/key_config.py
@@ -1,2 +1,9 @@
-ACCESS_KEY_ID='AKIATB7HFIZQNRTFM2U5'
-ACCESS_SECRET_KEY='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'
+# Abre el archivo y lee las líneas
+with open('keys.txt', 'r') as file:
+    lines = file.readlines()
+
+# Asume que la primera línea tiene el ACCESS_KEY_ID y la segunda tiene el ACCESS_SECRET_KEY
+ACCESS_KEY_ID = lines[0].strip()
+ACCESS_SECRET_KEY = lines[1].strip()
+print(ACCESS_KEY_ID)
+print(ACCESS_SECRET_KEY)
