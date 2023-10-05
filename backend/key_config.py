8ec8c66cebee09fcecbc9cebfa43ea443a548be9
Aaron Coorahua <ruben.coorahua@utec.edu.pe>
2023-10-05 13:04:44 -0500

    fix: se quita el debugging al config key

--- backend/key_config.py
+++ backend/key_config.py
@@ -5,5 +5,3 @@ with open('keys.txt', 'r') as file:
 # Asume que la primera línea tiene el ACCESS_KEY_ID y la segunda tiene el ACCESS_SECRET_KEY
 ACCESS_KEY_ID = lines[0].strip()
 ACCESS_SECRET_KEY = lines[1].strip()
-print(ACCESS_KEY_ID)
-print(ACCESS_SECRET_KEY)
