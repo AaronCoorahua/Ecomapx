9729d2cb9e79bde61094b4c3060ed94ee3773199
Aaron Coorahua <ruben.coorahua@utec.edu.pe>
2023-10-05 16:49:28 -0500

    feat: cambios provisionales en el login

--- backend/key_config.py
+++
@@ -1,7 +0,0 @@
-# Abre el archivo y lee las líneas
-with open('keys.txt', 'r') as file:
-    lines = file.readlines()
-
-# Asume que la primera línea tiene el ACCESS_KEY_ID y la segunda tiene el ACCESS_SECRET_KEY
-ACCESS_KEY_ID = lines[0].strip()
-ACCESS_SECRET_KEY = lines[1].strip()
